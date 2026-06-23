0ddb57a67e0629680ec9c34dcfb18d5a263fefcd
firoz <firozz0007@gmail.com>
2026-06-23 21:10:46 +0530

    done with data type

---
+++ data_type.java
@@ -0,0 +1,9 @@
+public class data_type {
+    public static void main(String[] args) {
+        String name="firoz";
+        int age=19;
+        double height=6.1;
+        System.out.println(name);        
+        System.out.println(age);        
+        System.out.print(height);    
+    }}
