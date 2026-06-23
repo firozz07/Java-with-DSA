58358d46432cc59fa6d4c2bb736ef9ce66fbb298
firoz <firozz0007@gmail.com>
2026-06-23 20:58:30 +0530

    variables done

--- variables.java
+++ variables.java
@@ -1,3 +1,11 @@
 public class variables {
-    
+    public static void main(String[] args) {
+        int x;
+        x=28;
+        System.out.println(x);
+        x=x*3;
+        System.out.println(x);
+        x=0;
+        System.out.println(x);
+    }
 }
