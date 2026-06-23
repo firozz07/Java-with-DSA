a4ad7e6df39481b4911f7885983662ddb6fe7a59
firoz <firozz0007@gmail.com>
2026-06-23 21:36:53 +0530

    scanner done

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a num");
+        int square=sc.nextInt();
+        System.out.print("square is : ");
+        System.out.println(square*square); 
+    }
+}
