38de2a04ba3b8ce842a374e5fde3f99efbc4197d
firoz <firozz0007@gmail.com>
2026-06-23 21:15:26 +0530

    area of circle

---
+++ area_circle.java
@@ -0,0 +1,9 @@
+public class area_circle {
+    public static void main(String[] args) {
+    double pi=3.14;
+    int r=5;
+    double area=pi*r*r;
+    System.out.print("area is ");
+    System.out.println(area);
+    }
+}
