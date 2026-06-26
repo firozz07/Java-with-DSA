e0c9f92b7a2f62ca48cd0978251c24bcfc5d3c71
firoz <firozz0007@gmail.com>
2026-06-26 17:53:33 +0530

    decrement and loops

---
+++ decrement.java
@@ -0,0 +1,9 @@
+public class decrement {
+    public static void main(String[] args) {
+        for (int i = 100; i >=1; i=i-2) {
+            if(i%3==0){
+            System.out.println(i);
+        }
+        }
+    }
+}
