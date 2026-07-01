f0619ea4a94f1b5869f3f905e93d4bc3277a3537
firoz <firozz0007@gmail.com>
2026-07-01 20:37:41 +0530

    fixed code

--- twoSumArray.java
+++ twoSumArray.java
@@ -1,21 +1,22 @@
 public class twoSumArray {
     public static void main(String[] args) {
         int [] arr={1,2,3,4,5};
-    int target=65;
+    int target=9;
     boolean found=false;
     for (int i = 0; i < arr.length; i++) {
-        for (int j = 0; j < arr.length; j++) {
+        for (int j = 1; j < arr.length; j++) {
             if(arr[i]+arr[j]==target) {
                 found=true;
+                System.out.print("the target is found at index :");
+                System.out.print(arr[i]+" & ");
+                System.out.print(arr[j]+" ");
+                break;
             }
-
         }
+        if(found) break;
     }
-    if(found){
-        System.out.println("the target is found");
-    }
-    else{
-        System.out.println("target doe not found");
+    if(found!=true){
+        System.out.print("the target is not found");
     }
     }
 
