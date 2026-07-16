34c159b1f7f6f42ff123032187dd65d66c3c5a49
firoz <firozz0007@gmail.com>
2026-07-16 16:08:30 +0530

    solved some examples

---
+++ Sorting Algorithm/selection Sort/eg.java
@@ -0,0 +1,17 @@
+public class eg {
+    public static void main(String[] args) {
+        int []arr={7,3,4,7,8,1};
+        for (int i = 0; i < arr.length-1; i++) {
+            int min=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j]<arr[min]){
+                    min=j;
+                    int temp=arr[i];
+                    arr[i]=arr[min];
+                    arr[min]=temp;
+                }
+            }
+        }
+        for(int i:arr) System.out.print(i+" ");
+    }
+}
